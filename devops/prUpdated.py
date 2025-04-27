b4aa79a922b038bb01fc805d47f81ff931471483
Pranay Jaiswal <pranaygreat@gmail.com>
2025-04-27 20:01:40 +0100

    Update prUpdated.py

--- devops/prUpdated.py
+++ devops/prUpdated.py
@@ -13,6 +13,11 @@ github_repository = os.environ.get('GITHUB_REPOSITORY')
 github_token = os.environ.get('TOKEN_GITHUB')
 commit_id = os.environ.get('COMMIT_ID')
 
+print(f"GitHub Repository: {github_repository}")
+print(f"GitHub Token: {github_token}")
+print(f"PR Number: {pr_number}")
+print(f"commit_id: {commit_id}")
+
 owner, repo = github_repository.split("/")
 
 deployment_result_file = "deploymentResult.json"
